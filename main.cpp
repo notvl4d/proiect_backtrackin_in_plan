023c7bc0c6cd9b4e0066fb102d8f0f3e22ca55d8
T-Flex09 <filiptiron0@gmail.com>
2025-11-10 09:47:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 //INDEXAM DE LA 1 APROPO
-int n, m, a[25][25], sol_I[1000], sol_j[1000], s_max = 0;
+int n, m, a[25][25], sol_i[1000], sol_j[1000], sol_len, s_max = 0;
 int poz_init_I, poz_init_J;
 int poz_fin_I, poz_fin_J;
 
@@ -25,17 +24,26 @@ void citire(){
 
 //VLADUTZ
 void back(int i, int j, int suma) {
+    sol_i[sol_len++] = i, sol_j[sol_len] = j;
+    //ceva ceva caz in care ajunge la destinatie
+    for(int k = 0; k < 4; k++) {
+        int vi = i + di[k];
+        int vj = j + dj[k];
+        if(0<=vi && vi<=n && 0<=vj && vj<=m) {
+            //ceva ceva conditie de parcurgere
+        }
+    }
     
+    sol_len--;
 } //parcurge matricea conform cerintei din poza
 
 //LARGEANU
 void tiparire(){
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=m; j++)
-            cout << a[i][j] << " ";
-    cout << endl;
+    for(int i = 1; i<=sol_len; i++) {
+        cout<<"("<<sol_i[i]<<", "<<sol_j[i]<<")";
+        if(i!=n) cout<<", ";
     }
-
+    cout<<"\n";
 } //tipareste vectorul de solutii
 
 int main() {
